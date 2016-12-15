5fe5c2988482bbe51aeca379510513b6bdcec85e
Remi PUCCINELLI <rpuccine@e1r9p11.42.fr>
2016-12-15 19:17:53 +0100

    fix indentation

--- dir_serveur/includes/serveur.h
+++ dir_serveur/includes/serveur.h
@@ -37,118 +37,118 @@ typedef struct in_addr      IN_ADDR;
 /*
 ** ************************ Network **************************
 */
-typedef struct 							s_network_data
+typedef struct 				s_network_data
 {
-	int												port;
-	SOCKET										sock_endpoint;
-	SOCKET										sock_max;
-}														t_network_data;
+	int						port;
+	SOCKET					sock_endpoint;
+	SOCKET					sock_max;
+}							t_network_data;
 
 /*
 ** ************************ Team *****************************
 */
-typedef struct 							s_team_entity
+typedef struct 				s_team_entity
 {
-	char											*name;
-	int												available_slots;
-}														t_team_entity;
+	char					*name;
+	int						available_slots;
+}							t_team_entity;
 
-typedef struct 							s_team_hdl
+typedef struct 				s_team_hdl
 {
-	int												nb_teams;
-	t_team_entity							*array_teams;
-}														t_team_hdl;
+	int						nb_teams;
+	t_team_entity			*array_teams;
+}							t_team_hdl;
 
 /*
 ** ************************ Client **************************
 */
-typedef struct 							s_client_entity
+typedef struct 				s_client_entity
 {
-	SOCKET                    sock;
-	t_team_entity            	*team;
-	char                      buff[BUFF_SIZE];
-	int                       start_buff;
-	int                       len_buff;
-	int												nb_pending_cmds;
-	t_list_cmds_entity				*list_pending_cmds;
-}														t_client_entity;
+	SOCKET					sock;
+	t_team_entity			*team;
+	char					buff[BUFF_SIZE];
+	int						start_buff;
+	int						len_buff;
+	int						nb_pending_cmds;
+	t_list_cmds_entity		*list_pending_cmds;
+}							t_client_entity;
 
-typedef struct							s_list_clients_entity
+typedef struct				s_list_clients_entity
 {
-	t_client_entity						*client;
-	t_list_clients_entity			*next;
-}														t_list_clients_entity;
+	t_client_entity			*client;
+	t_list_clients_entity	*next;
+}							t_list_clients_entity;
 
-typedef struct 							s_client_hdl
+typedef struct 				s_client_hdl
 {
-	int												nb_clients;
-	t_list_clients_entity			*list_clients;
-}														t_client_hdl;
+	int						nb_clients;
+	t_list_clients_entity	*list_clients;
+}							t_client_hdl;
 
 /*
 ** ************************ Cmds *****************************
 */
-typedef struct 							s_cmd_entity
+typedef struct 				s_cmd_entity
 {
-	char											*name;
-	int												available_slots;
-}														t_cmd_entity;
+	char					*name;
+	int						available_slots;
+}							t_cmd_entity;
 
-typedef struct 							s_list_cmds_entity
+typedef struct 				s_list_cmds_entity
 {
-	t_cmd_entity							*cmd;
-	t_list_cmds_entity				*next
-}														t_list_cmds_entity;
+	t_cmd_entity			*cmd;
+	t_list_cmds_entity		*next
+}							t_list_cmds_entity;
 
-typedef struct 							s_cmd_hdl
+typedef struct 				s_cmd_hdl
 {
-	int												nb_cmds;
-	t_client_entity						*array_cmds;
-}														t_cmd_hdl;
+	int						nb_cmds;
+	t_client_entity			*array_cmds;
+}							t_cmd_hdl;
 
 /*
 ** ******************** Serveur Main Struct ******************
 */
-typedef struct							s_serveur
+typedef struct				s_serveur
 {
-	t_network_data 						network;
-	t_team_hdl								team_hdl;
-	t_client_hdl							client_hdl;
-	t_cmd_hdl									cmd_hdl;
-	t_world_hdl								world_hdl;
-}														t_serveur;
+	t_network_data 			network;
+	t_team_hdl				team_hdl;
+	t_client_hdl			client_hdl;
+	t_cmd_hdl				cmd_hdl;
+	t_world_hdl				world_hdl;
+}							t_serveur;
 
-//*********************************************************************
+//***********************OLD TOUT POURRRS**********************************************
 
 // recupéré dans input_handling.c
-typedef struct			        s_serv_settings
+typedef struct				s_serv_settings
 {
-	int					              port;
-	int					              map_width;
-	int					              map_height;
-	int					              nb_of_teams;
-	char				              **team_names;
-	int					              nb_of_clients;
-	int					              t;
-}                           t_serv_settings;
+	int						port;
+	int						map_width;
+	int						map_height;
+	int						nb_of_teams;
+	char					**team_names;
+	int						nb_of_clients;
+	int						t;
+}							t_serv_settings;
 
-typedef struct              s_client
+typedef struct				s_client
 {
-  SOCKET                    sock;
-  char                      team[BUFF_SIZE];
-  char                      buff[BUFF_SIZE];
-  int                       start_buff;
-  int                       len_buff;
-}                           t_client;
+  SOCKET					sock;
+  char						team[BUFF_SIZE];
+  char						buff[BUFF_SIZE];
+  int						start_buff;
+  int						len_buff;
+}							t_client;
 
-typedef struct              s_server_data
+typedef struct				s_server_data
 {
-  SOCKET                    sock_endpoint;
-  int                       last_sock;
-  t_client                  list_clients[MAX_CLIENT];
-  int                       nb_clients;
-  t_serv_settings           serv_settings;
-}                           t_server_data;
+  SOCKET					sock_endpoint;
+  int						last_sock;
+  t_client					list_clients[MAX_CLIENT];
+  int						nb_clients;
+  t_serv_settings			serv_settings;
+}							t_server_data;
 
 /*
 ** input_handler.c
