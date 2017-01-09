8f1f2198be4b47eef85997720f4ab37edae4acf0
aleung-c <aleung-c@e1r8p9.42.fr>
2017-01-09 17:03:51 +0100

    add gfx refusal if set + todo: corriger segfault lorsque client est disconnected

--- dir_serveur/src/client_recognition.c
+++ dir_serveur/src/client_recognition.c
@@ -16,30 +16,50 @@ void	client_recognition(t_serveur *serv, t_client_entity *client)
 	{
 		if (strcmp(buff, "GRAPHIC\n") == 0)
 		{
-			client->is_gfx = 1;
-			serv->client_hdl.gfx_client = client;
-			// sending world size;
-			asprintf(&str_to_send, "msz %d %d\n", serv->world_hdl.map_x, serv->world_hdl.map_y);
-			write_buffer(&client->buff_send, str_to_send, strlen(str_to_send));
-			free(str_to_send);
-			printf(KGRN "GFX client recognized\n" KRESET);
+			if (!serv->client_hdl.gfx_client)
+			{
+				serv->client_hdl.gfx_client = client;
+				client->is_gfx = 1;
+				// sending world size;
+				asprintf(&str_to_send, "msz %d %d\n", serv->world_hdl.map_x, serv->world_hdl.map_y);
+				write_buffer(&client->buff_send, str_to_send, strlen(str_to_send));
+				free(str_to_send);
+				printf(KGRN "GFX client recognized\n" KRESET);
+			}
+			else
+			{
+				printf(KRED "GFX client already set\n" KRESET);
+				// TODO: serveur segfault here. idem en dessous.
+				remove_client(serv, client); // ???
+				disconnect_client(client->sock); // ???
+				client = serv->client_hdl.list_clients; // set ptr back to beginning.
+				return ;
+			}
 		}
 		else
 		{
+			
 			if (!(team = get_team_by_name(serv, buff)))
 			{
-				printf("Get_team() failed\n");
+				// TODO: server segfault ici lorsque disconnect player.
+				printf(KRED "Get_team() failed\n" KRESET);
 				write_buffer(&client->buff_send, "UNKNOWN TEAM\n", 10);
-				disconnect_client(client->sock);
+				remove_client(serv, client); // ???
+				disconnect_client(client->sock); // ???
+				client = serv->client_hdl.list_clients; // set ptr back to beginning.
 				free(buff);
 				return ;
 			}
 			// sending first datas; <nb-client>\n<x><y>\n
 			client->is_in_game = 1;
+			client->team = team;
 			client->buff_recv.len = 0;
 			printf(KGRN "Player client recognized\n" KRESET);
 			asprintf(&str_to_send, "%d\n%d %d\n", team->available_slots, serv->world_hdl.map_x, serv->world_hdl.map_y);
 			write_buffer(&client->buff_send, str_to_send, strlen(str_to_send));
+
+			// one slot now taken in team.
+			team->available_slots -= 1;
 		}
 	}
 }
