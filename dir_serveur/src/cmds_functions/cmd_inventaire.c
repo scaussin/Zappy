de12e9384b5f78eabbb1c85559663caccbe6ad73
aleung-c <aleung-c@e1r10p9.42.fr>
2017-02-02 18:49:26 +0100

    ui avancee + inventaire food time remaining now displayed

--- dir_serveur/src/cmds_functions/cmd_inventaire.c
+++ dir_serveur/src/cmds_functions/cmd_inventaire.c
@@ -6,6 +6,9 @@ void	cmd_inventaire(struct s_serveur *serv, struct s_client_entity *client_cur, 
 	(void)				serv;
 	t_player			*cur_player;
 	char				*msg;
+	long				time_left;
+	struct timespec		timespec_life_left;
+	struct timespec		now;
 
 	cur_player = &(client_cur->player);
 
@@ -15,9 +18,24 @@ void	cmd_inventaire(struct s_serveur *serv, struct s_client_entity *client_cur, 
 					"from sock %d\n" KRESET, client_cur->sock);
 		return ;
 	}
+	// we set the time at food - 1 for the untouched food
+	time_left = (cur_player->inventory[FOOD] - 1) * FOOD_LIFE_TIME;
+
+	// now we want the status of the current food.
+	get_time(&now);
+	timespec_life_left = timespec_diff(&now, &cur_player->next_dinner_time);
+
+	// Time conversion to nanoseconds for precise time remaining.
+	while (timespec_life_left.tv_sec > 0)
+	{
+		timespec_life_left.tv_sec -= 1;
+		timespec_life_left.tv_nsec += 1000000000;
+	}
+	// calculation to turn remaining time into (int)t_unit time.
+	time_left += (int)roundf((float)(timespec_life_left.tv_nsec / (float)1000000000) / serv->world_hdl.t_unit);
 	// player client response.
-	asprintf(&msg, "{nourriture %d, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d}\n",  // nourriture ??
-		cur_player->inventory[FOOD],
+	asprintf(&msg, "{nourriture %ld, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d}\n",  // nourriture ??
+		time_left,
 		cur_player->inventory[LINEMATE],
 		cur_player->inventory[DERAUMERE],
 		cur_player->inventory[SIBUR],
