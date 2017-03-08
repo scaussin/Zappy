0489b9077e979debcc32a51690a1d5c5b50eb452
aleung-c <aleung-c@e1r13p4.42.fr>
2017-03-08 14:46:58 +0100

    push gfx exec

--- dir_gfx/src/gfx_main.c
+++ dir_gfx/src/gfx_main.c
@@ -3,11 +3,25 @@
 #include <string.h>
 #include <stdlib.h>
 
-int		main(void)
+/*
+** We take the beginning of argv[0] until the last '/' and
+** append the path to our main scene to open unity.
+*/
+
+int		main(int argc, char **argv)
 {
   char *command;
-  
-  asprintf(&command, "open gfx_unity_project/Assets/Scenes/gfx.unity");
-  system(command);
+  char *path;
+  int i;
+
+  i = 0;
+  if (argc == 1 && argv[0])
+    {
+      path = strrchr(argv[0], '/');
+      *path = '\0';
+      asprintf(&command, "open %s/gfx_unity_project/Assets/Scenes/gfx.unity", argv[0]);
+      system(command);
+      free(command);
+    }
   return (0);
 }
